313c4006af3d4f2b04cbaa95dac59d593b082803
abhilash <abhilash.kamble376@gmail.com>
2022-09-22 22:40:43 +0530

    resolved add category and customer update password

--- src/customer/component/CustomerChangePassword.js
+++ src/customer/component/CustomerChangePassword.js
@@ -1,45 +1,44 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from 'axios'
-import {useHistory,useLocation } from 'react-router-dom'
-const url = 'http://localhost:8080'
+import { useHistory, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
-const CustomerChangePassword=()=>{
-    const[ password ,setnewPassword]=useState('')
-    const[ id ,setid]=useState('')
-    const[users ,setUsers]=useState({})
-    const history = useHistory()
-   
-    const user= JSON.parse(localStorage.getItem('user'))  
-    
-   
-    const ChangePassword=(id)=>{
-        axios.put(`http://localhost:8080/user/update/${id}`,{'password':password}).then((response)=>{
-          alert("success")
-          history.push('/signin-signup')
-        })
-}
-return (
-      <div className="container">
-        <input
-                onChange={(e) => {
-                    setid(e.target.value)
-                }}
-                className="form-control" type="hidden" />
-         {/* <button className="btn btn-success"onClick={getAllfromdb}>Show User</button> */}
-         
-           {
-          <h1>Email: {user.email}</h1> 
-         
-         }
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter new Password.." onChange={(e)=>{
-            setnewPassword(e.target.value)
-            }}/>
-            <button type="submit" class="btn btn-primary" onClick={()=>{ChangePassword(user.id)}}>Submit</button>
-         </div>
-         
-      
-);
+const CustomerChangePassword = () => {
+  const [password, setnewPassword] = useState('')
+  const [id, setid] = useState('')
+  const history = useHistory()
+
+  const user = JSON.parse(localStorage.getItem('user'))
+  const userSignIn = useSelector(state => state?.userSignIn)
+
+
+  const ChangePassword = (id) => {
+    axios.put(`http://localhost:8080/user/update/${id}`, { 'password': password }).then((response) => {
+      alert("success")
+      history.push('/signin-signup')
+    })
+  }
+  return (
+    <div className="container">
+      <input
+        onChange={(e) => {
+          setid(e.target.value)
+        }}
+        className="form-control" type="hidden" />
+
+      {
+        <h1>Email: {user.email}</h1>
+
+      }
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter new Password.." onChange={(e) => {
+        setnewPassword(e.target.value)
+      }} />
+      <button type="submit" class="btn btn-primary" onClick={() => { ChangePassword(userSignIn[0]?.id) }}>Submit</button>
+    </div>
+
+
+  );
 }
 
 export default CustomerChangePassword
